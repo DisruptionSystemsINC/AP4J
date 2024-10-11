4539cfa8e541302188a35c5f6bb42f10b838f73e
disruption <leon.beer@t-online.de>
2024-10-12 00:24:38 +0200

    Initial Commit of Proof of Concept code

---
+++ src/main/java/com/disruption/AnyPub4J/API/AccountObjectBuildHelper.java
@@ -0,0 +1,10 @@
+package com.disruption.AnyPub4J.API;
+
+import com.disruption.AnyPub4J.AP4J;
+import com.disruption.AnyPub4J.Objects.Account;
+
+public class AccountObjectBuildHelper {
+    public Account createAccount(AP4J ap4J){
+
+    }
+}
