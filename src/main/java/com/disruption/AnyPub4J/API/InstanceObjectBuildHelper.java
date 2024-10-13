0e13e71b762492b1bd91b40411a1c6fa083261be
disruption <leon.beer@t-online.de>
2024-10-13 11:35:02 +0200

    Merge branch 'master' of https://github.com/DisruptionSystemsINC/AP4J No You ass

---
+++ src/main/java/com/disruption/AnyPub4J/API/InstanceObjectBuildHelper.java
@@ -0,0 +1,9 @@
+package com.disruption.AnyPub4J.API;
+
+import com.disruption.AnyPub4J.Objects.Instance;
+
+public class InstanceObjectBuildHelper {
+    public Instance buildInstance(){
+        return new Instance();
+            }
+}
--- src/main/java/com/disruption/AnyPub4J/Objects/Instance.java
+++ src/main/java/com/disruption/AnyPub4J/Objects/Instance.java
@@ -1,7 +1,7 @@
 package com.disruption.AnyPub4J.Objects;
 
 public class Instance {
-    Instance(){
+    public Instance(){
 
     }
 }
